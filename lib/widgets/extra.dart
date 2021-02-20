4334c8f50686d127bc8af4a88e9ed87e04e41bd6
SinaMN75 <sina75mn@outlook.com>
2021-02-20 15:10:22 +0330

    :)

--- lib/widgets/extra.dart
+++ lib/widgets/extra.dart
@@ -1,7 +1,7 @@
 part of '../widgets.dart';
 
 Widget divider({double width, double height, Color color = Colors.grey, EdgeInsets padding}) => Container(
-      padding: padding,
+      margin: padding,
       width: width != null ? width : screenWidth,
       height: height != null ? height : 1,
       color: color,
