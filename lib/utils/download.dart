dc206a72e14fa6758abc09c94e75cecea07962e2
SinaMN75 <sina75mn@outlook.com>
2021-03-12 23:14:06 +0330

    :)

--- lib/utils/download.dart
+++ lib/utils/download.dart
@@ -1,9 +1,10 @@
 part of '../utilities.dart';
 
-downloadImage({@required List<String> imageUrls}) async {
-  imageUrls.forEach((i) async => await get(
+downloadImage({@required List<String> imageUrls, Function action}) async => imageUrls.forEach((i) async {
+      await get(
         url: i,
         action: (response) async => await ImageGallerySaver.saveImage(response.bodyBytes, name: DateTime.now().toString()),
         error: (response) {},
-      ));
-}
+      );
+      if (i == imageUrls.last) action();
+    });
