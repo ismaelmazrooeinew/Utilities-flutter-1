e17da6a3ee098519656c26bdd683776c0e27f351
SinaMN75 <sina75mn@outlook.com>
2021-03-13 17:14:19 +0330

    fix local storage

--- lib/utils/local_storage.dart
+++ lib/utils/local_storage.dart
@@ -2,14 +2,29 @@ part of '../utilities.dart';
 
 Future setData(String key, dynamic value) async => Hive.box(key).put(key, value);
 
-Future<int> getInt(String key) async => Hive.box(key).get(key);
+Future<int> getInt(String key) async {
+  Hive.openBox(key);
+  return Hive.box(key).get(key);
+}
 
-Future<bool> getBool(String key) async => Hive.box(key).get(key);
+Future<bool> getBool(String key) async {
+  Hive.openBox(key);
+  return Hive.box(key).get(key);
+}
 
-Future<double> getDouble(String key) async => Hive.box(key).get(key);
+Future<double> getDouble(String key) async {
+  Hive.openBox(key);
+  return Hive.box(key).get(key);
+}
 
-Future<String> getString(String key) async => Hive.box(key).get(key);
+Future<String> getString(String key) async {
+  Hive.openBox(key);
+  return Hive.box(key).get(key);
+}
 
-Future<dynamic> getData(String key) async => Hive.box(key).get(key);
+Future<dynamic> getData(String key) async {
+  Hive.openBox(key);
+  return Hive.box(key).get(key);
+}
 
 Future clearData() async => await Hive.deleteFromDisk();
