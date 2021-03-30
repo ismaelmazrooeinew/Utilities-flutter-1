ec906b7eb96f0426552ec6b8f77a6f73a5ff9201
SinaMN75 <sina75mn@outlook.com>
2021-03-30 12:13:37 +0430

    breaking changes on text

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -2,182 +2,220 @@ part of '../widgets.dart';
 
 Widget textHeadLine1(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.headline1.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline1 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textHeadLine2(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.headline2.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textHeadLine3(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.headline3.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textHeadLine4(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.headline4.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textHeadLine5(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.headline5.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textHeadLine6(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.headline6.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textSubtitle1(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.subtitle1.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textSubtitle2(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.subtitle2.copyWith(fontSize: fontSize, color: color, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textBody1(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
-  int maxLines,
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.bodyText1.copyWith(color: color, fontSize: fontSize, fontWeight: fontWeight),
-          textAlign: textAlign,
-          maxLines: maxLines,
-          overflow: TextOverflow.ellipsis,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textBody2(
   String text, {
-  Color color,
-  double fontSize,
-  FontWeight fontWeight,
+  int maxLines,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.bodyText2.copyWith(color: color, fontSize: fontSize, fontWeight: fontWeight),
-          textAlign: textAlign,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
 
 Widget textCaption(
   String text, {
-  Color color,
-  double fontSize,
   int maxLines,
-  FontWeight fontWeight,
+  TextOverflow textOverflow,
+  TextStyle style,
   TextAlign textAlign = TextAlign.justify,
   EdgeInsets padding: const EdgeInsets.all(0),
 }) =>
     Padding(
         padding: padding,
-        child: Text(
-          text,
-          style: theme.textTheme.caption.copyWith(color: color, fontSize: fontSize, fontWeight: fontWeight),
-          textAlign: textAlign,
-          maxLines: maxLines,
-          overflow: maxLines == null ? null : TextOverflow.ellipsis,
-        ));
+        child: Text(text,
+            style: style == null ? theme.textTheme.headline2 : style,
+            textAlign: textAlign,
+            maxLines: maxLines,
+            overflow: maxLines == null
+                ? null
+                : textOverflow == null
+                    ? TextOverflow.ellipsis
+                    : textOverflow));
