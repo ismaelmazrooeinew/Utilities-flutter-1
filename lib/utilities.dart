e22f53ecde29e6ae813c3794a49a0ad2617dfc5c
Sina MohammadZadeh <sina75mn@outlook.com>
2021-08-02 11:14:31 +0430

    fix launch

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -27,6 +27,7 @@ export 'package:intl/intl.dart';
 export 'package:meta/meta.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:carousel_slider/carousel_slider.dart';
+export 'package:share/share.dart';
 
 part 'data/datasource.dart';
 
