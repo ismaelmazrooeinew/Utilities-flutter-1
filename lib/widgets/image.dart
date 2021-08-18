751c96ae3d48d4cc706acb06a9496b687c337dca
Sina MohammadZadeh <sina75mn@outlook.com>
2021-08-18 20:16:38 +0430

    add image radius

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -7,22 +7,30 @@ Widget imageAsset(
   double? height,
   BoxFit fit = BoxFit.contain,
   Clip clipBehavior = Clip.hardEdge,
+  double borderRadius = 1,
+  EdgeInsets margin = EdgeInsets.zero,
 }) =>
-    asset.substring(asset.length - 3).toLowerCase() == "svg"
-        ? SvgPicture.asset(
-            asset,
-            color: color,
-            width: width,
-            height: height,
-            fit: fit,
-            clipBehavior: clipBehavior,
-          )
-        : Image.asset(
-            asset,
-            color: color,
-            width: width,
-            height: height,
-            cacheWidth: width?.toInt(),
-            cacheHeight: height?.toInt(),
-            fit: fit,
-          );
+    Padding(
+      padding: margin,
+      child: radius(
+        child: asset.substring(asset.length - 3).toLowerCase() == "svg"
+            ? SvgPicture.asset(
+                asset,
+                color: color,
+                width: width,
+                height: height,
+                fit: fit,
+                clipBehavior: clipBehavior,
+              )
+            : Image.asset(
+                asset,
+                color: color,
+                width: width,
+                height: height,
+                cacheWidth: width?.toInt(),
+                cacheHeight: height?.toInt(),
+                fit: fit,
+              ),
+        radius: borderRadius,
+      ),
+    );
