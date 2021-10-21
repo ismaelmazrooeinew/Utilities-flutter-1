0f91f6f4f79a66772e1274ce7b017d857e2b28d6
Sina MohammadZadeh <sina75mn@outlook.com>
2021-10-21 21:56:34 +0330

    code clean

--- lib/utils/http_interceptor.dart
+++ lib/utils/http_interceptor.dart
@@ -3,71 +3,79 @@ part of '../utilities.dart';
 GetConnect getConnect = GetConnect(
   followRedirects: true,
   timeout: Duration(minutes: 60),
+  allowAutoSignedCert: true,
+  sendUserAgent: true,
+  userAgent: "ThisIsFrozenSun",
+  maxRedirects: 3,
+  maxAuthRetries: 3,
 );
 
 Future<void> request(
   String url,
   EHttpMethod httpMethod,
-  action(Response? response),
-  error(Response? response), {
+  action(Response response),
+  error(Response response), {
   body,
 }) async {
-  final String? token = getString(Constant.token);
+  Map<String, String> headers = {
+    "Content-Type": "application/json",
+    "Authorization": getString(Constant.token) ?? "",
+  };
 
-  Map<String, String> headers = {"Content-Type": "application/json", "Authorization": token ?? ""};
-
-  Response? response;
+  Response response = Response();
   if (httpMethod == EHttpMethod.get) response = await getConnect.get(url, headers: headers);
   if (httpMethod == EHttpMethod.post) response = await getConnect.post(url, body, headers: headers);
   if (httpMethod == EHttpMethod.put) response = await getConnect.put(url, body, headers: headers);
   if (httpMethod == EHttpMethod.patch) response = await getConnect.patch(url, body, headers: headers);
   if (httpMethod == EHttpMethod.delete) response = await getConnect.delete(url, headers: headers);
 
-  if (body != null)
-    response!.completeLog(params: body.toJson());
-  else
-    response!.log();
-  if (response.isSuccessful())
+  if (body != null) {
+    response.completeLog(params: body.toJson());
+  } else {
+    response.log();
+  }
+  if (response.isSuccessful()) {
     action(response);
-  else
+  } else {
     error(response);
+  }
 }
 
 Future<void> get({
   required String url,
-  required action(Response? response),
-  required error(Response? response),
+  required action(Response response),
+  required error(Response response),
 }) async =>
     await request(url, EHttpMethod.get, action, error);
 
 Future<void> post({
   required String url,
-  required action(Response? response),
-  required error(Response? response),
+  required action(Response response),
+  required error(Response response),
   body,
 }) async =>
     await request(url, EHttpMethod.post, action, error, body: body);
 
 Future<void> put({
   required String url,
-  required action(Response? response),
-  required error(Response? response),
+  required action(Response response),
+  required error(Response response),
   body,
 }) async =>
     await request(url, EHttpMethod.put, action, error, body: body);
 
 Future<void> patch({
   required String url,
-  required action(Response? response),
-  required error(Response? response),
+  required action(Response response),
+  required error(Response response),
   body,
 }) async =>
     await request(url, EHttpMethod.patch, action, error, body: body);
 
 Future<void> delete({
   required String url,
-  required action(Response? response),
-  required error(Response? response),
+  required action(Response response),
+  required error(Response response),
 }) async =>
     await request(url, EHttpMethod.delete, action, error);
 
@@ -78,9 +86,9 @@ extension HTTP on Response {
 
   bool isServerError() => (this.statusCode ?? 0) >= 500 && (this.statusCode ?? 0) <= 599 ? true : false;
 
-  log() => print("${this.request!.method} - ${this.request!.url} - ${this.statusCode} - RESPONSE: ${this.body}");
+  void log() => print("${this.request!.method} - ${this.request!.url} - ${this.statusCode} - RESPONSE: ${this.body}");
 
-  completeLog({String? params}) => print(
+  void completeLog({String? params}) => print(
         "${this.request!.method} - ${this.request!.url} - ${this.statusCode} HEADERS: ${this.headers} - PARAMS: $params - RESPONSE: ${this.body}",
       );
 }
