fe452b2d5cfc82832216018c007d5cdafa6d9619
SinaMN75 <Sia75MN@outlook.com>
2021-11-12 11:34:06 +0330

    :(

--- lib/utils/file.dart
+++ lib/utils/file.dart
@@ -1,20 +1,20 @@
 part of '../utilities.dart';
 
-// void showFilePicker({
-//   required Function(List<File> file) action,
-//   FileType fileType = FileType.image,
-//   bool allowMultiple = false,
-// }) async {
-//   FilePickerResult? result = await FilePicker.platform.pickFiles(type: fileType, allowMultiple: allowMultiple);
-//   if (result != null) {
-//     if (allowMultiple) {
-//       List<File> files = <File>[];
-//       result.files.forEach((i) {
-//         if (i.path != null) files.add(File(i.path!));
-//       });
-//     } else {
-//       File file = File(result.files.single.path!);
-//       action([file]);
-//     }
-//   }
-// }
+void showFilePicker({
+  required Function(List<File> file) action,
+  FileType fileType = FileType.image,
+  bool allowMultiple = false,
+}) async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles(type: fileType, allowMultiple: allowMultiple);
+  if (result != null) {
+    if (allowMultiple) {
+      List<File> files = <File>[];
+      result.files.forEach((i) {
+        if (i.path != null) files.add(File(i.path!));
+      });
+    } else {
+      File file = File(result.files.single.path!);
+      action([file]);
+    }
+  }
+}
