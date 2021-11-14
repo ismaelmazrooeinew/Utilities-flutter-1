0045c13550dcaea42ca40dbe72339accdc37d36d
SinaMN75 <Sia75MN@outlook.com>
2021-11-14 18:58:07 +0330

    :(

--- lib/utils/file.dart
+++ lib/utils/file.dart
@@ -18,25 +18,3 @@ void showFilePicker({
     }
   }
 }
-
-
-class UtilitiesFile {
-  void showFilePicker({
-    required Function(List<File> file) action,
-    FileType fileType = FileType.image,
-    bool allowMultiple = false,
-  }) async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(type: fileType, allowMultiple: allowMultiple);
-    if (result != null) {
-      if (allowMultiple) {
-        List<File> files = <File>[];
-        result.files.forEach((i) {
-          if (i.path != null) files.add(File(i.path!));
-        });
-      } else {
-        File file = File(result.files.single.path!);
-        action([file]);
-      }
-    }
-  }
-}
