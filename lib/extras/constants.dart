7ebc15447c81cd0a7d99df5ebef3fcd9ec3173de
SinaMN75 <Sia75MN@outlook.com>
2021-11-18 23:18:36 +0330

    :(

--- lib/extras/constants.dart
+++ lib/extras/constants.dart
@@ -1,10 +1,31 @@
 part of '../utilities.dart';
 
 extension PageStateExtension on PageState {
-  bool isLoading() => this == PageState.loading;
-  bool isLoaded() => this == PageState.loaded;
   bool isInitial() => this == PageState.initial;
+
+  bool isLoading() => this == PageState.loading;
+
+  bool isLoaded() => this == PageState.loaded;
+
   bool isError() => this == PageState.error;
+}
+
+extension RxPageStateExtension on Rx<PageState> {
+  bool isLoading() => this.value == PageState.loading;
+
+  bool isLoaded() => this.value == PageState.loaded;
+
+  bool isInitial() => this.value == PageState.initial;
+
+  bool isError() => this.value == PageState.error;
+
+  PageState initial() => this(PageState.initial);
+
+  PageState loading() => this(PageState.loading);
+
+  PageState loaded() => this(PageState.loaded);
+
+  PageState error() => this(PageState.error);
 }
 
 class Array {
@@ -27,8 +48,10 @@ enum PageState {
 }
 
 class Sample {
-  static const persianLorem = "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.";
-  static const lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  static const persianLorem =
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.";
+  static const lorem =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
   static const profileImageUrl = "https://www.himalmag.com/wp-content/uploads/2019/07/sample-profile-picture.png";
   static const sampleName1 = "سینا محمدزاده نوری";
   static const loremPicsum = "https://picsum.photos/seed/picsum/200/300";
