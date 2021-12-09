5982162a5557d9bc956c447cc5cc58d05b0ba028
SinaMN75 <82322615+NahidLaCiura@users.noreply.github.com>
2021-12-09 22:10:48 +0330

    :(

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,14 +1,14 @@
 import 'dart:ui';
 
-// extension IterableExtentions on Iterable {
-//   Iterable<E> mapIndexed<E, T>(E Function(int index, T item) f) sync* {
-//     int index = 0;
-//     for (final item in this) {
-//       yield f(index, item);
-//       index = index + 1;
-//     }
-//   }
-// }
+extension IterableExtentions on Iterable {
+  Iterable<E> mapIndexed<E, T>(E Function(int index, T item) f) sync* {
+    int index = 0;
+    for (final item in this) {
+      yield f(index, item);
+      index = index + 1;
+    }
+  }
+}
 
 extension BoolExtensios on String {
   bool isTrue() => toLowerCase() == 'true';
