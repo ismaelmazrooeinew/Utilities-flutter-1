92d6167cbec4c08bd214491ffe8ddfa2663a0fbf
SinaMN75 <82322615+NahidLaCiura@users.noreply.github.com>
2021-12-10 00:00:35 +0330

    :)

--- lib/utils/device_settings.dart
+++ lib/utils/device_settings.dart
@@ -1,33 +1,33 @@
-import 'package:app_settings/app_settings.dart';
+// import 'package:app_settings/app_settings.dart';
 
 /// see: https://pub.dev/packages/app_settings
 
-void openAppSettings() => AppSettings.openAppSettings();
-
-void openBatteryOptimizationSettings() => AppSettings.openBatteryOptimizationSettings();
-
-void openBluetoothSettings() => AppSettings.openBluetoothSettings();
-
-void openDataRoamingSettings() => AppSettings.openDataRoamingSettings();
-
-void openDateSettings() => AppSettings.openDateSettings();
-
-void openDeviceSettings() => AppSettings.openDeviceSettings();
-
-void openDisplaySettings() => AppSettings.openDisplaySettings();
-
-void openInternalStorageSettings() => AppSettings.openInternalStorageSettings();
-
-void openLocationSettings() => AppSettings.openLocationSettings();
-
-void openNFCSettings() => AppSettings.openNFCSettings();
-
-void openNotificationSettings() => AppSettings.openNotificationSettings();
-
-void openSecuritySettings() => AppSettings.openSecuritySettings();
-
-void openSoundSettings() => AppSettings.openSoundSettings();
-
-void openVPNSettings() => AppSettings.openVPNSettings();
-
-void openWIFISettings() => AppSettings.openWIFISettings();
+// void openAppSettings() => AppSettings.openAppSettings();
+//
+// void openBatteryOptimizationSettings() => AppSettings.openBatteryOptimizationSettings();
+//
+// void openBluetoothSettings() => AppSettings.openBluetoothSettings();
+//
+// void openDataRoamingSettings() => AppSettings.openDataRoamingSettings();
+//
+// void openDateSettings() => AppSettings.openDateSettings();
+//
+// void openDeviceSettings() => AppSettings.openDeviceSettings();
+//
+// void openDisplaySettings() => AppSettings.openDisplaySettings();
+//
+// void openInternalStorageSettings() => AppSettings.openInternalStorageSettings();
+//
+// void openLocationSettings() => AppSettings.openLocationSettings();
+//
+// void openNFCSettings() => AppSettings.openNFCSettings();
+//
+// void openNotificationSettings() => AppSettings.openNotificationSettings();
+//
+// void openSecuritySettings() => AppSettings.openSecuritySettings();
+//
+// void openSoundSettings() => AppSettings.openSoundSettings();
+//
+// void openVPNSettings() => AppSettings.openVPNSettings();
+//
+// void openWIFISettings() => AppSettings.openWIFISettings();
