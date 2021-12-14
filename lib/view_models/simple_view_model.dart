502e44ee852011ae98a9f26600603b4c873f8c32
SinaMN75 <82322615+NahidLaCiura@users.noreply.github.com>
2021-12-14 23:07:46 +0330

    :(

---
+++ lib/view_models/simple_view_model.dart
@@ -0,0 +1,17 @@
+class SimpleViewModel {
+  SimpleViewModel({
+    this.id,
+    this.title,
+    this.subtitle,
+    this.description,
+    this.link,
+    this.imageUrl,
+  });
+
+  final String? id;
+  final String? title;
+  final String? subtitle;
+  final String? description;
+  final String? link;
+  final String? imageUrl;
+}
