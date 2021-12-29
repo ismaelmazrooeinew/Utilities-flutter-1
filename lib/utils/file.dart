c24e012c1e7d8456df1e09b0b9d27e57fd529fe5
SinaMN75 <82322615+NahidLaCiura@users.noreply.github.com>
2021-12-29 11:59:51 +0330

    :(

--- lib/utils/file.dart
+++ lib/utils/file.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:utilities/utilities.dart';
 
 void showFilePicker({
   required final Function(List<File> file) action,
@@ -19,4 +21,49 @@ void showFilePicker({
       action(<File>[file]);
     }
   }
+}
+
+Future<File?> cropImage({
+  required File file,
+  Function(File file)? action,
+  int? maxWidth,
+  int? maxHeight,
+  CropAspectRatio cropAspectRatio = const CropAspectRatio(ratioX: 3.0, ratioY: 1.2),
+  ImageCompressFormat imageCompressFormat = ImageCompressFormat.png,
+  AndroidUiSettings? androidUiSettings,
+  IOSUiSettings? iOSUiSettings,
+}) async {
+  final File? result = await ImageCropper.cropImage(
+    sourcePath: file.path,
+    maxWidth: maxWidth,
+    maxHeight: maxHeight,
+    aspectRatio: cropAspectRatio,
+    compressFormat: imageCompressFormat,
+    cropStyle: CropStyle.rectangle,
+    compressQuality: 90,
+    androidUiSettings: androidUiSettings ??
+        AndroidUiSettings(
+          toolbarTitle: 'Crop Your Image',
+          showCropGrid: true,
+          hideBottomControls: false,
+          lockAspectRatio: true,
+          initAspectRatio: CropAspectRatioPreset.square,
+          activeControlsWidgetColor: context.theme.primaryColor,
+          statusBarColor: context.theme.primaryColor,
+          toolbarColor: context.theme.primaryColor,
+          toolbarWidgetColor: context.theme.cardColor,
+        ),
+    iosUiSettings: iOSUiSettings ??
+        IOSUiSettings(
+          resetAspectRatioEnabled: false,
+          minimumAspectRatio: 1.0,
+          aspectRatioPickerButtonHidden: true,
+          title: 'Crop Your Image',
+          aspectRatioLockDimensionSwapEnabled: true,
+          aspectRatioLockEnabled: true,
+          hidesNavigationBar: true,
+        ),
+  );
+  if (action != null) action(result!);
+  return result;
 }
