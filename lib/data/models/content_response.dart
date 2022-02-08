1420bc726a6e18030e6ac13b61198f8c189fcd80
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-02-08 19:34:34 +0330

    :(

--- lib/data/models/content_response.dart
+++ lib/data/models/content_response.dart
@@ -6,6 +6,7 @@ class ContentResponse {
     required this.title,
     required this.subTitle,
     required this.description,
+    required this.link,
     required this.media,
     required this.contactInformation,
     required this.useCase,
@@ -18,6 +19,7 @@ class ContentResponse {
         title: json["Title"],
         subTitle: json["SubTitle"],
         description: json["Description"],
+        link: json["Link"],
         media: json["Media"] == null ? null : List<MediaResponse>.from(json["Media"].map((final dynamic x) => MediaResponse.fromMap(x))),
         contactInformation: json["ContactInformation"] == null ? null : List<ContactInformationResponse>.from(json["ContactInformation"].map((final dynamic x) => ContactInformationResponse.fromMap(x))),
         useCase: json["UseCase"],
@@ -30,6 +32,7 @@ class ContentResponse {
         "Title": title,
         "SubTitle": subTitle,
         "Description": description,
+        "Link": link,
         "Media": media == null ? null : List<dynamic>.from(media!.map((final dynamic x) => x.toMap())),
         "ContactInformation": contactInformation == null ? null : List<dynamic>.from(contactInformation!.map((final dynamic x) => x.toMap())),
         "UseCase": useCase,
@@ -40,6 +43,7 @@ class ContentResponse {
   final String? title;
   final String? subTitle;
   final String? description;
+  final String? link;
   final List<MediaResponse>? media;
   final List<ContactInformationResponse>? contactInformation;
 }
