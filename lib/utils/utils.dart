8d2970757bca692623acf3add6ff1d49a02df199
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-02-11 11:16:44 +0330

    :(

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,7 +2,17 @@ import 'dart:ui';
 
 import 'package:logger/logger.dart';
 
-Logger logger = Logger();
+Logger logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 1,
+    errorMethodCount: 4,
+    lineLength: 400,
+    colors: true,
+    printEmojis: true,
+    printTime: false,
+    noBoxingByDefault: true,
+  ),
+);
 
 void delay(final int milliseconds, final VoidCallback action) async => Future<dynamic>.delayed(
       Duration(milliseconds: milliseconds),
