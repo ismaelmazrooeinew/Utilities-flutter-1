4de9c231311c4a4da90e168d50b739b3a016fb35
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-02-20 07:45:56 +0330

    amend global location path

--- lib/data/local_datasource/local_datasource.dart
+++ lib/data/local_datasource/local_datasource.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart' show rootBundle;
 
 class LocalDataSource {
   Future<List<LocationResponse>> getLocations() async {
-    final String data = await rootBundle.loadString("lib/assets/data/global_locations.json");
+    final String data = await rootBundle.loadString("assets/data/global_locations.json");
     final List<LocationResponse> list = <LocationResponse>[];
     for (final dynamic i in jsonDecode(data)) list.add(LocationResponse.fromMap(i));
     return list;
