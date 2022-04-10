2ef3305f50f931943a3bf08b2516e3ecd65561eb
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-04-10 19:33:57 +0430

    add firebase

--- lib/utils/firebase/authentication.dart
+++ lib/utils/firebase/authentication.dart
@@ -1,26 +1,26 @@
-// import 'package:flutter/material.dart';
-// import 'package:utilities/utilities.dart';
-//
-// /// Setup Firebase Auth in project first.
-// /// Add SHA-1 ad SHA-256 in google PlayStore
-//
-// Future<void> signInByGoogle({
-//   required Function(GoogleSignInAccount, UserCredential) onSignIn,
-//   required VoidCallback onCancel,
-// }) async {
-//   final GoogleSignInAccount? account = await GoogleSignIn().signIn();
-//   if (account == null) {
-//     onCancel();
-//     return;
-//   }
-//
-//   final GoogleSignInAuthentication googleAuth = await account.authentication;
-//
-//   final OAuthCredential credential = GoogleAuthProvider.credential(
-//     accessToken: googleAuth.accessToken,
-//     idToken: googleAuth.idToken,
-//   );
-//
-//   final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
-//   onSignIn(account, userCredential);
-// }
+import 'package:flutter/material.dart';
+import 'package:utilities/utilities.dart';
+
+/// Setup Firebase Auth in project first.
+/// Add SHA-1 ad SHA-256 in google PlayStore
+
+Future<void> signInByGoogle({
+  required Function(GoogleSignInAccount, UserCredential) onSignIn,
+  required VoidCallback onCancel,
+}) async {
+  final GoogleSignInAccount? account = await GoogleSignIn().signIn();
+  if (account == null) {
+    onCancel();
+    return;
+  }
+
+  final GoogleSignInAuthentication googleAuth = await account.authentication;
+
+  final OAuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+  onSignIn(account, userCredential);
+}
