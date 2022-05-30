2d506ee8d4675ad49e28aa0c2c35ae5ca6b5ee4d
Mahmoud Mahmoudi <mahmoudmahmoudi.ir@gmail.com>
2022-05-30 11:34:14 +0430

    Revert "Merge branch 'mahmoud'"
    This reverts commit 4ff89de6e16bfbfc0e10fdc365009417a0e1c3ce, reversing
    changes made to b85e71ec7e77943ae4d21421947e5e308b51e99c.

--- lib/utils/extensions/widget_extension.dart
+++ lib/utils/extensions/widget_extension.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:utilities/utilities.dart';
 
 extension WidgetsExtension on Widget {
   Widget withTooltip(
@@ -27,16 +26,4 @@ extension WidgetsExtension on Widget {
   Widget fit() => FittedBox(key: key, child: this, fit: BoxFit.scaleDown);
 
   Widget expanded({final int flex = 1}) => Expanded(flex: flex, child: this);
-
-  Widget obx({
-    final Rx<PageState>? state,
-    final Widget loading = const Center(child: CircularProgressIndicator()),
-  }) =>
-      Obx(
-        () => state == null
-            ? this
-            : state.isLoaded()
-                ? this
-                : loading,
-      );
 }
