d1a9c1889af3426fdd70f6abad3e71702e2ef8e0
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-02 21:45:44 +0430

    removed view models

--- lib/view_models/speciality_view_model.dart
+++
@@ -1,8 +0,0 @@
-class SpecialityViewModel {
-  SpecialityViewModel({required this.id, required this.title, this.imageUrl = "", this.placeholder});
-
-  final int id;
-  final String title;
-  final String imageUrl;
-  final String? placeholder;
-}
--- lib/view_models/view_models.dart
+++ lib/view_models/view_models.dart
@@ -1,3 +1,1 @@
-export 'contact_information_view_model.dart';
 export 'media_view_model.dart';
-export 'speciality_view_model.dart';
