2a39d0ca147636e44d382572d65a2877d7fdca5e
Marzieh Ghodrat <ma.ghodrat@gmail.com>
2022-06-12 01:14:26 +0430

    add notification request

---
+++ lib/data/dto/notification.dart
@@ -0,0 +1,49 @@
+import 'package:utilities/utilities.dart';
+
+class NotificationReadDto {
+  NotificationReadDto({
+    this.id,
+    this.title,
+    this.message,
+    this.link,
+    this.createdAt,
+    this.visited,
+    this.useCase,
+    this.media,
+  });
+
+  final String? id;
+  final String? title;
+  final String? message;
+  final String? link;
+  final String? createdAt;
+  final bool? visited;
+  final int? useCase;
+  final List<MediaReadDto>? media;
+
+  factory NotificationReadDto.fromJson(String str) => NotificationReadDto.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory NotificationReadDto.fromMap(Map<String, dynamic> json) => NotificationReadDto(
+    id: json["id"] == null ? null : json["id"],
+    title: json["title"] == null ? null : json["title"],
+    message: json["message"] == null ? null : json["message"],
+    link: json["link"] == null ? null : json["link"],
+    createdAt: json["createdAt"] == null ? null :json["createdAt"],
+    visited: json["visited"] == null ? null : json["visited"],
+    useCase: json["useCase"] == null ? null : json["useCase"],
+    media: json["media"] == null ? null : List<MediaReadDto>.from(json["media"].map((x) => MediaReadDto.fromMap(x))),
+  );
+
+  Map<String, dynamic> toMap() => {
+    "id": id == null ? null : id,
+    "title": title == null ? null : title,
+    "message": message == null ? null : message,
+    "link": link == null ? null : link,
+    "createdAt": createdAt == null ? null : createdAt,
+    "visited": visited == null ? null : visited,
+    "useCase": useCase == null ? null : useCase,
+    "media": media == null ? null : List<dynamic>.from(media!.map((x) => x.toMap())),
+  };
+}
---
+++ lib/data/remote_datasource/notification_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:utilities/data/dto/notification.dart';
+import 'package:utilities/utilities.dart';
+
+class NotificationDataSource {
+  final String baseUrl;
+
+  NotificationDataSource({required this.baseUrl});
+
+  Future<void> createNotification({
+    required final NotificationReadDto dto,
+    required final Function(GenericResponse<NotificationReadDto>) onResponse,
+    required final Function(GenericResponse response) onError,
+  }) async =>
+      post(
+        url: "$baseUrl/Notification",
+        body: dto,
+        action: (Response response) => onResponse(GenericResponse.fromMap(response.body)),
+        error: (Response response) => onError(GenericResponse.fromMap(response.body)),
+      );
+
+  Future<void> readNotification({
+    required final Function(GenericResponse<NotificationReadDto>) onResponse,
+    required final Function(GenericResponse response) onError,
+  }) async =>
+      get(
+        url: "$baseUrl/Notification",
+        action: (Response response) => onResponse(GenericResponse.fromMap(response.body)),
+        error: (Response response) => onError(GenericResponse.fromMap(response.body)),
+      );
+
+}
