6bcde47518f4571b555f7a74f4e1a2c63f8d8661
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-15 12:51:54 +0430

    add gql

--- lib/utils/graph_ql.dart
+++ lib/utils/graph_ql.dart
@@ -12,7 +12,7 @@ Future<void> query({
   required String url,
   required String query,
   required Function(Map<String, dynamic>? result) action,
-  required Function(Map<String, dynamic>?) error,
+  required Function(String) error,
   FetchPolicy fetchPolicy = FetchPolicy.networkOnly,
 }) async {
   QueryResult res = await _getGraphQlClient(
@@ -20,7 +20,7 @@ Future<void> query({
     accessToken: getString(UtilitiesConstants.token),
   ).query(QueryOptions(document: gql(query), fetchPolicy: fetchPolicy));
   if (res.hasException)
-    error(res.data);
+    error(res.exception!.graphqlErrors[0].message);
   else
     action(res.data);
 }
@@ -29,7 +29,7 @@ Future<void> mutation({
   required String url,
   required String query,
   required Function(Map<String, dynamic>? result) action,
-  required Function(Map<String, dynamic>?) error,
+  required Function(String) error,
   FetchPolicy fetchPolicy = FetchPolicy.networkOnly,
 }) async {
   QueryResult res = await _getGraphQlClient(
@@ -37,7 +37,7 @@ Future<void> mutation({
     accessToken: getString(UtilitiesConstants.token),
   ).mutate(MutationOptions(document: gql(query), fetchPolicy: fetchPolicy));
   if (res.hasException)
-    error(res.data);
+    error(res.exception!.graphqlErrors[0].message);
   else
     action(res.data);
 }
