a106eddb617c854645ad5a6310cc4f6468270081
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-17 00:16:16 +0430

    add form dto

--- lib/data/dto/form.dart
+++ lib/data/dto/form.dart
@@ -2,13 +2,13 @@ import 'package:utilities/utilities.dart';
 
 class FormFieldDto {
   FormFieldDto({
-     this.id,
-     this.label,
-     this.title,
-     this.isRequired,
-     this.optionList,
-     this.type,
-     this.categoryId,
+    this.id,
+    this.label,
+    this.title,
+    this.isRequired,
+    this.optionList,
+    this.type,
+    this.categoryId,
   });
 
   final String? id;
@@ -41,5 +41,93 @@ class FormFieldDto {
         "optionList": optionList == null ? null : optionList,
         "type": type == null ? null : type,
         "categoryId": categoryId == null ? null : categoryId,
+      };
+}
+
+class FormCreateUpdateDto {
+  FormCreateUpdateDto({
+    this.userId,
+    this.productId,
+    this.projectId,
+    this.tutorialId,
+    this.eventId,
+    this.adId,
+    this.companyId,
+    this.tenderId,
+    this.serviceId,
+    this.magazineId,
+    this.form,
+  });
+
+  final String? userId;
+  final String? productId;
+  final String? projectId;
+  final String? tutorialId;
+  final String? eventId;
+  final String? adId;
+  final String? companyId;
+  final String? tenderId;
+  final String? serviceId;
+  final String? magazineId;
+  final List<Form>? form;
+
+  factory FormCreateUpdateDto.fromJson(String str) => FormCreateUpdateDto.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory FormCreateUpdateDto.fromMap(Map<String, dynamic> json) => FormCreateUpdateDto(
+        userId: json["userId"] == null ? null : json["userId"],
+        productId: json["productId"] == null ? null : json["productId"],
+        projectId: json["projectId"] == null ? null : json["projectId"],
+        tutorialId: json["tutorialId"] == null ? null : json["tutorialId"],
+        eventId: json["eventId"] == null ? null : json["eventId"],
+        adId: json["adId"] == null ? null : json["adId"],
+        companyId: json["companyId"] == null ? null : json["companyId"],
+        tenderId: json["tenderId"] == null ? null : json["tenderId"],
+        serviceId: json["serviceId"] == null ? null : json["serviceId"],
+        magazineId: json["magazineId"] == null ? null : json["magazineId"],
+        form: json["form"] == null ? null : List<Form>.from(json["form"].map((x) => Form.fromMap(x))),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "userId": userId == null ? null : userId,
+        "productId": productId == null ? null : productId,
+        "projectId": projectId == null ? null : projectId,
+        "tutorialId": tutorialId == null ? null : tutorialId,
+        "eventId": eventId == null ? null : eventId,
+        "adId": adId == null ? null : adId,
+        "companyId": companyId == null ? null : companyId,
+        "tenderId": tenderId == null ? null : tenderId,
+        "serviceId": serviceId == null ? null : serviceId,
+        "magazineId": magazineId == null ? null : magazineId,
+        "form": form == null ? null : List<dynamic>.from(form!.map((x) => x.toMap())),
+      };
+}
+
+class Form {
+  Form({
+    required this.id,
+    required this.title,
+    required this.formField,
+  });
+
+  final String? id;
+  final String? title;
+  final FormFieldDto? formField;
+
+  factory Form.fromJson(String str) => Form.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Form.fromMap(Map<String, dynamic> json) => Form(
+        id: json["id"] == null ? null : json["id"],
+        title: json["title"] == null ? null : json["title"],
+        formField: json["formField"] == null ? null : FormFieldDto.fromMap(json["formField"]),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id == null ? null : id,
+        "title": title == null ? null : title,
+        "formField": formField == null ? null : formField!.toMap(),
       };
 }
