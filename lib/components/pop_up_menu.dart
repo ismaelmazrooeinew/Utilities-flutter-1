63acf2676a9346932dd34af70f566e962eab5ae4
Marzieh Ghodrat <ma.ghodrat@gmail.com>
2022-06-19 03:19:54 +0430

    add pop up menu

---
+++ lib/components/pop_up_menu.dart
@@ -0,0 +1,68 @@
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
+import '../utilities.dart';
+import 'package:flutter/material.dart';
+
+Widget popupMenu({
+  required final Widget Function({
+  required CustomPopupMenuController controller,
+  })
+  menuItems,
+  required final Widget child,
+  final PressType pressType = PressType.singleClick,
+  final double verticalMargin = 0,
+  final Color arrowColor=Colors.white,
+}) {
+  final CustomPopupMenuController _customPopupMenuController = CustomPopupMenuController();
+  return CustomPopupMenu(
+    pressType: pressType,
+    verticalMargin: verticalMargin,
+    arrowColor: arrowColor,
+    arrowSize: 12,
+    menuBuilder: () => menuItems(controller: _customPopupMenuController),
+    controller: _customPopupMenuController,
+    child: child,
+  );
+}
+
+Widget popupMenuItem({
+  required final CustomPopupMenuController controller,
+  required final Widget title,
+  required final Widget icon,
+  required final VoidCallback onTap,
+  final EdgeInsets contentPadding=const EdgeInsets.only(left: 10, right: 2),
+  final double horizontalTitleGap=4,
+  final Key? key,
+}) =>
+    ListTile(
+        minVerticalPadding: 0,
+        minLeadingWidth: 0,
+        dense: true,
+        contentPadding:contentPadding,
+        visualDensity: const VisualDensity(vertical: -2),
+        horizontalTitleGap:horizontalTitleGap,
+        selected: true,
+        leading: icon,
+        title:title,
+        onTap: () {
+          controller.hideMenu();
+          onTap();
+        });
+
+Widget listMenu({
+  required final CustomPopupMenuController controller,
+  required final double width,
+  required final List<Widget> children,
+  final double borderRadius=12,
+  final double padding=8,
+  final Color color=Colors.white,
+}) =>
+    radius(
+      radius: borderRadius,
+      child: column(
+        width: width,
+        padding: EdgeInsets.all(padding),
+        decoration: BoxDecoration(color: color),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: children,
+      ),
+    );
