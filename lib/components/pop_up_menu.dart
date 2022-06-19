fec3d9190bda9387139e8b4fd1a4d6ea8978f816
Marzieh Ghodrat <ma.ghodrat@gmail.com>
2022-06-19 10:30:29 +0430

    update pop up menu

--- lib/components/pop_up_menu.dart
+++ lib/components/pop_up_menu.dart
@@ -3,14 +3,12 @@ import '../utilities.dart';
 import 'package:flutter/material.dart';
 
 Widget popupMenu({
-  required final Widget Function({
-  required CustomPopupMenuController controller,
-  })
-  menuItems,
   required final Widget child,
+  required final double width,
+  required final List<Widget> children,
   final PressType pressType = PressType.singleClick,
   final double verticalMargin = 0,
-  final Color arrowColor=Colors.white,
+  final Color arrowColor = Colors.white,
 }) {
   final CustomPopupMenuController _customPopupMenuController = CustomPopupMenuController();
   return CustomPopupMenu(
@@ -18,7 +16,11 @@ Widget popupMenu({
     verticalMargin: verticalMargin,
     arrowColor: arrowColor,
     arrowSize: 12,
-    menuBuilder: () => menuItems(controller: _customPopupMenuController),
+    menuBuilder: () => listOfMenuItems(
+      controller: _customPopupMenuController,
+      width: width,
+      children: children,
+    ),
     controller: _customPopupMenuController,
     child: child,
   );
@@ -29,32 +31,32 @@ Widget popupMenuItem({
   required final Widget title,
   required final Widget icon,
   required final VoidCallback onTap,
-  final EdgeInsets contentPadding=const EdgeInsets.only(left: 10, right: 2),
-  final double horizontalTitleGap=4,
+  final EdgeInsets contentPadding = const EdgeInsets.only(left: 10, right: 2),
+  final double horizontalTitleGap = 4,
   final Key? key,
 }) =>
     ListTile(
         minVerticalPadding: 0,
         minLeadingWidth: 0,
         dense: true,
-        contentPadding:contentPadding,
+        contentPadding: contentPadding,
         visualDensity: const VisualDensity(vertical: -2),
-        horizontalTitleGap:horizontalTitleGap,
+        horizontalTitleGap: horizontalTitleGap,
         selected: true,
         leading: icon,
-        title:title,
+        title: title,
         onTap: () {
           controller.hideMenu();
           onTap();
         });
 
-Widget listMenu({
+Widget listOfMenuItems({
   required final CustomPopupMenuController controller,
   required final double width,
   required final List<Widget> children,
-  final double borderRadius=12,
-  final double padding=8,
-  final Color color=Colors.white,
+  final double borderRadius = 12,
+  final double padding = 8,
+  final Color color = Colors.white,
 }) =>
     radius(
       radius: borderRadius,
