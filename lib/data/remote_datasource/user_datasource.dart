0224b6d891286df2daf6e0d94361fdcfa862bca8
Mahmoud Mahmoudi <mahmoudmahmoudi.ir@gmail.com>
2022-06-19 07:55:35 +0430

    Update user_datasource.dart

--- lib/data/remote_datasource/user_datasource.dart
+++ lib/data/remote_datasource/user_datasource.dart
@@ -100,13 +100,13 @@ class UserDataSource {
   Future<void> verifyMobileForLogin({
     required final VerifyMobileForLoginDto dto,
     required final Function(GenericResponse<UserReadDto>) onResponse,
-    required final Function(GenericResponse response) onError,
+    required final Function(GenericResponse<UserReadDto> response) onError,
   }) async =>
       httpPost(
         url: "$baseUrl/user/VerifyMobileForLogin",
         body:dto,
         action: (Response response) => onResponse(GenericResponse<UserReadDto>.fromJson(response.body, UserReadDto.fromMap)),
-        error: (Response response) => onError(GenericResponse<String>.fromJson(response.body, UserReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse<UserReadDto>.fromJson(response.body, UserReadDto.fromMap)),
       );
 
   Future<void> getProfile({
