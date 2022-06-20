8e1cdb3308ea3c5a0c7e4bfa063706621a59aa97
Marzieh Ghodrat <ma.ghodrat@gmail.com>
2022-06-20 12:17:05 +0430

    update media dto

--- lib/data/dto/media.dart
+++ lib/data/dto/media.dart
@@ -2,38 +2,38 @@ import 'package:utilities/utilities.dart';
 
 class MediaReadDto {
   MediaReadDto({
-    this.id = -1,
-    this.type = -1,
-    this.useCase = -1,
-    this.link = "-1",
-    this.title = "-1"
+    this.id='-1',
+    this.type=-1,
+    this.useCase='',
+    this.link='',
+    this.title='',
   });
+
+  final String id;
+  final int type;
+  final String useCase;
+  final String link;
+  final String title;
+
+  factory MediaReadDto.fromJson(String str) => MediaReadDto.fromMap(json.decode(str));
 
   String toJson() => json.encode(toMap());
 
+  factory MediaReadDto.fromMap(Map<String, dynamic> json) => MediaReadDto(
+    id: json["id"] == null ? null : json["id"],
+    type: json["type"] == null ? null : json["type"],
+    useCase: json["useCase"] == null ? null : json["useCase"],
+    link: json["link"] == null ? null : json["link"],
+    title: json["title"] == null ? null : json["title"],
+  );
+
   Map<String, dynamic> toMap() => {
-        "id": id == null ? null : id,
-        "type": type == null ? null : type,
-        "useCase": useCase == null ? null : useCase,
-        "link": link == null ? null : link,
-        "title": title == null ? null : title,
-      };
-
-  factory MediaReadDto.fromMap(final Map<String, dynamic> json) => MediaReadDto(
-        id: json["Id"] ?? -1,
-        type: json["Type"] ?? -1,
-        useCase: json["UseCase"] ?? -1,
-        link: json["Link"] ?? "-1",
-        title: json["Title"] ?? "-1",
-      );
-
-  factory MediaReadDto.fromJson(final String str) => MediaReadDto.fromMap(json.decode(str));
-
-  final int? id;
-  final int? type;
-  final int? useCase;
-  final String link;
-  final String? title;
+    "id": id == null ? null : id,
+    "type": type == null ? null : type,
+    "useCase": useCase == null ? null : useCase,
+    "link": link == null ? null : link,
+    "title": title == null ? null : title,
+  };
 }
 
 extension NullableMediaResponseExtension on List<MediaReadDto>? {
