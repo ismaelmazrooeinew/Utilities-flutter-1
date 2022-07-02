f591a5aeb85e40812526a9fe278e7b774ba785c4
Ahmad <ahmadxpanahi@gmail.com>
2022-07-02 09:56:12 +0430

    Add global search data source

---
+++ lib/data/remote_datasource/global_search_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:utilities/data/dto/global_search.dart';
+import 'package:utilities/utilities.dart';
+
+class GlobalSearchDataSource{
+
+  final String baseUrl;
+
+  GlobalSearchDataSource({required this.baseUrl});
+
+  Future<void> create({
+    required final GlobalSearchCreateDto dto,
+    required final Function(GenericResponse<GlobalSearchReadDto>) onResponse,
+    required final Function(GenericResponse response) onError,
+  }) async =>
+      httpPost(
+        url: "$baseUrl/GlobalSearch",
+        body: dto,
+        action: (Response response) => onResponse(GenericResponse<GlobalSearchReadDto>.fromJson(response.body, GlobalSearchReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse<dynamic>.fromJson(response.body, ContentReadDto.fromMap)),
+      );
+
+
+}
