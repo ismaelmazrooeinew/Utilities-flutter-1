0d87ad6df7ca64016bedfa3ad8cf96e4843c5b91
arash <Arashmohmdi79@gmail.com>
2022-07-22 19:03:49 +0430

    Update notification.dart

--- lib/data/dto/notification.dart
+++ lib/data/dto/notification.dart
@@ -5,20 +5,20 @@ class NotificationReadDto {
     this.id,
     this.title,
     this.message,
-    this.link,
     this.createdAt,
     this.visited,
     this.useCase,
+    this.creatorUser,
     this.media,
   });
 
   final String? id;
   final String? title;
   final String? message;
-  final String? link;
   final String? createdAt;
   final bool? visited;
   final String? useCase;
+  final UserReadDto? creatorUser;
   final List<MediaReadDto>? media;
 
   factory NotificationReadDto.fromJson(String str) => NotificationReadDto.fromMap(json.decode(str));
@@ -29,10 +29,10 @@ class NotificationReadDto {
         id: json["id"] == null ? null : json["id"],
         title: json["title"] == null ? null : json["title"],
         message: json["message"] == null ? null : json["message"],
-        link: json["link"] == null ? null : json["link"],
         createdAt: json["createdAt"] == null ? null : json["createdAt"],
         visited: json["visited"] == null ? null : json["visited"],
         useCase: json["useCase"] == null ? null : json["useCase"],
+        creatorUser: json["creatorUser"] == null ? null : UserReadDto.fromMap(json["creatorUser"]),
         media: json["media"] == null ? null : List<MediaReadDto>.from(json["media"].map((x) => MediaReadDto.fromMap(x))),
       );
 
@@ -40,10 +40,10 @@ class NotificationReadDto {
         "id": id == null ? null : id,
         "title": title == null ? null : title,
         "message": message == null ? null : message,
-        "link": link == null ? null : link,
         "createdAt": createdAt == null ? null : createdAt,
         "visited": visited == null ? null : visited,
         "useCase": useCase == null ? null : useCase,
+        "creatorUser": creatorUser == null ? null : creatorUser!.toMap(),
         "media": media == null ? null : List<dynamic>.from(media!.map((x) => x.toMap())),
       };
 }
