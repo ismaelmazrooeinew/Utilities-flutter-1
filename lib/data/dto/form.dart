68a68612a356dcf71625f6088aa0d35dd40b9c30
Ahmad <ahmadxpanahi@gmail.com>
2022-07-26 18:26:36 +0430

    Edit form field dto

--- lib/data/dto/form.dart
+++ lib/data/dto/form.dart
@@ -1,23 +1,16 @@
+import 'package:utilities/data/dto/form_filed.dart';
 import 'package:utilities/utilities.dart';
 
 class FormReadDto {
   FormReadDto({
     this.id,
-    this.label,
     this.title,
-    this.isRequired,
-    this.optionList,
-    this.type,
-    this.categoryId,
+    this.formField,
   });
 
   final String? id;
-  final String? label;
   final String? title;
-  final String? optionList;
-  final String? categoryId;
-  final bool? isRequired;
-  final int? type;
+  final FormFieldReadDto? formField;
 
   factory FormReadDto.fromJson(String str) => FormReadDto.fromMap(json.decode(str));
 
@@ -25,22 +18,14 @@ class FormReadDto {
 
   factory FormReadDto.fromMap(Map<String, dynamic> json) => FormReadDto(
         id: json["id"] == null ? null : json["id"],
-        label: json["label"] == null ? null : json["label"],
         title: json["title"] == null ? null : json["title"],
-        isRequired: json["isRequired"] == null ? null : json["isRequired"],
-        optionList: json["optionList"] == null ? null : json["optionList"],
-        type: json["type"] == null ? null : json["type"],
-        categoryId: json["categoryId"] == null ? null : json["categoryId"],
+        formField: json["formField"] == null ? null : FormFieldReadDto.fromMap(json["formField"]),
       );
 
   Map<String, dynamic> toMap() => {
         "id": id == null ? null : id,
-        "label": label == null ? null : label,
         "title": title == null ? null : title,
-        "isRequired": isRequired == null ? null : isRequired,
-        "optionList": optionList == null ? null : optionList,
-        "type": type == null ? null : type,
-        "categoryId": categoryId == null ? null : categoryId,
+        "formField": formField == null ? null : formField!.toMap(),
       };
 }
 
---
+++ lib/data/dto/form_filed.dart
@@ -0,0 +1,45 @@
+import 'package:utilities/utilities.dart';
+
+class FormFieldReadDto {
+  FormFieldReadDto({
+    this.id,
+    this.label,
+    this.title,
+    this.isRequired,
+    this.optionList,
+    this.type,
+    this.categoryId,
+  });
+
+  final String? id;
+  final String? label;
+  final String? title;
+  final String? optionList;
+  final String? categoryId;
+  final bool? isRequired;
+  final int? type;
+
+  factory FormFieldReadDto.fromJson(String str) => FormFieldReadDto.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory FormFieldReadDto.fromMap(Map<String, dynamic> json) => FormFieldReadDto(
+    id: json["id"] == null ? null : json["id"],
+    label: json["label"] == null ? null : json["label"],
+    title: json["title"] == null ? null : json["title"],
+    isRequired: json["isRequired"] == null ? null : json["isRequired"],
+    optionList: json["optionList"] == null ? null : json["optionList"],
+    type: json["type"] == null ? null : json["type"],
+    categoryId: json["categoryId"] == null ? null : json["categoryId"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "id": id == null ? null : id,
+    "label": label == null ? null : label,
+    "title": title == null ? null : title,
+    "isRequired": isRequired == null ? null : isRequired,
+    "optionList": optionList == null ? null : optionList,
+    "type": type == null ? null : type,
+    "categoryId": categoryId == null ? null : categoryId,
+  };
+}
