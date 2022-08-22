011afa6da62b02bd8ef43aaeeaee3205076a43b2
Ahmad <ahmadxpanahi@gmail.com>
2022-08-23 01:42:12 +0430

    Add iran.json

--- lib/data/local_datasource/local_datasource.dart
+++ lib/data/local_datasource/local_datasource.dart
@@ -3,7 +3,7 @@ import 'package:utilities/utilities.dart';
 
 class LocalDataSource {
   Future<List<LocationReadDto>> getLocations() async {
-    final String data = await rootBundle.loadString("packages/utilities/lib/assets/data/global_locations.json");
+    final String data = await rootBundle.loadString("packages/utilities/lib/assets/data/iran.json");
     final List<LocationReadDto> list = <LocationReadDto>[];
     for (final dynamic i in jsonDecode(data)) list.add(LocationReadDto.fromMap(i));
     return list;
