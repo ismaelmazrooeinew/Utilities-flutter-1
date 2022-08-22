499dc9131acc7e472033ce7145a102cb3c56c206
Ahmad <ahmadxpanahi@gmail.com>
2022-08-23 01:54:17 +0430

    Add iran location dto

--- lib/data/local_datasource/local_datasource.dart
+++ lib/data/local_datasource/local_datasource.dart
@@ -3,9 +3,16 @@ import 'package:utilities/utilities.dart';
 
 class LocalDataSource {
   Future<List<LocationReadDto>> getLocations() async {
-    final String data = await rootBundle.loadString("packages/utilities/lib/assets/data/iran.json");
+    final String data = await rootBundle.loadString("packages/utilities/lib/assets/data/global_locations.json");
     final List<LocationReadDto> list = <LocationReadDto>[];
     for (final dynamic i in jsonDecode(data)) list.add(LocationReadDto.fromMap(i));
+    return list;
+  }
+
+  Future<List<IranLocationReadDto>> getIranLocations() async {
+    final String data = await rootBundle.loadString("packages/utilities/lib/assets/data/iran.json");
+    final List<IranLocationReadDto> list = <IranLocationReadDto>[];
+    for (final dynamic i in jsonDecode(data)) list.add(IranLocationReadDto.fromMap(i));
     return list;
   }
 }
