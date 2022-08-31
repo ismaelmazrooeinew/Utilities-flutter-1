88e88f90935d72175850eae13380794285a390f2
Farzad <104139314+farzaddev65@users.noreply.github.com>
2022-08-31 14:45:18 +0430

    test location

--- lib/data/local_datasource/local_datasource.dart
+++ lib/data/local_datasource/local_datasource.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:utilities/utilities.dart';
 
@@ -11,11 +10,7 @@ class LocalDataSource {
   }
 
   Future<List<IranLocationReadDto>> getIranLocations() async {
-
-    String data = await
-    DefaultAssetBundle.of(context).loadString("packages/utilities/lib/assets/data/iran.json");
-    // final String data = await rootBundle.loadString("packages/utilities/lib/assets/data/iran.json");
-
+    final String data = await rootBundle.loadString("packages/utilities/lib/assets/data/iran.json");
     final List<IranLocationReadDto> list = <IranLocationReadDto>[];
     for (final dynamic i in json.decode(data)) list.add(IranLocationReadDto.fromMap(i));
     return list;
