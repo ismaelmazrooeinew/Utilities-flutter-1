57a24ac1cd0662aabaddbaf725644c125c7321d9
Ahmad <92666998+ahmadxpanahi@users.noreply.github.com>
2022-09-17 23:24:36 +0530

    Update notification_datasource.dart

--- lib/data/remote_datasource/notification_datasource.dart
+++ lib/data/remote_datasource/notification_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/animation.dart';
 import 'package:utilities/utilities.dart';
 
 class NotificationDataSource {
@@ -12,6 +13,20 @@ class NotificationDataSource {
       httpGet(
         url: "$baseUrl/Notification",
         action: (Response response) => onResponse(GenericResponse<NotificationReadDto>.fromJson(response.body, fromMap: NotificationReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse.fromJson(response.body)),
+      );
+
+  Future<void> updateSeenStatus({
+    required final List<String> notificationIds,
+    required final int status,
+    required final VoidCallback onResponse,
+    required final Function(GenericResponse response) onError,
+  }) async =>
+      httpPost(
+        encodeBody: false,
+        url: "$baseUrl/Notification/UpdateSeenStatus?seenStatus=$status",
+        body: notificationIds,
+        action: (Response response) => onResponse(),
         error: (Response response) => onError(GenericResponse.fromJson(response.body)),
       );
 }
