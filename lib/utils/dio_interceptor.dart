3aee7a6bcc460c4914e810c8851be16b3ec59dad
Farzad <104139314+farzaddev65@users.noreply.github.com>
2022-10-05 14:02:19 +0330

    Edit dio_datasource 6

--- lib/utils/dio_interceptor.dart
+++ lib/utils/dio_interceptor.dart
@@ -4,28 +4,28 @@ import 'package:utilities/utils/http_interceptor.dart';
 import 'package:utilities/utils/local_storage.dart';
 
 Future<void> request(
-    final String url,
-    final EHttpMethod httpMethod,
-    final Function(Response<dynamic> response) action,
-    final Function(Response<dynamic> response) error, {
-      final String? queryOrMutation,
-      final dynamic body,
-      final bool encodeBody = true,
-      final Map<String, String>? headers,
-      final String userAgent = 'SinaMN75',
-      final bool followRedirects = true,
-      final Duration timeout = const Duration(minutes: 60),
-      final int maxRedirects = 5,
-      final bool allowAutoSignedCert = false,
-      final bool sendUserAgent = false,
-      final int maxAuthRetries = 1,
-      final bool withCredentials = false,
-    }) async {
+  final String url,
+  final EHttpMethod httpMethod,
+  final Function(Response<dynamic> response) action,
+  final Function(Response<dynamic> response) error, {
+  final String? queryOrMutation,
+  final dynamic body,
+  final bool encodeBody = true,
+  final Map<String, String>? headers,
+  final String userAgent = 'SinaMN75',
+  final bool followRedirects = true,
+  final Duration timeout = const Duration(minutes: 60),
+  final int maxRedirects = 5,
+  final bool allowAutoSignedCert = false,
+  final bool sendUserAgent = false,
+  final int maxAuthRetries = 1,
+  final bool withCredentials = false,
+}) async {
   final Map<String, String> header = <String, String>{"Authorization": getString(UtilitiesConstants.token) ?? ""};
 
   if (headers != null) header.addAll(headers);
-
-  Response<dynamic> response;
+  final Dio dio = Dio();
+  Response response = await dio.get(url, options: Options(headers: header));
   try {
     dynamic params;
     if (body != null) {
@@ -35,67 +35,64 @@ Future<void> request(
         params = body;
     }
 
-    final Dio dio = Dio();
+    // if (httpMethod == EHttpMethod.get) {
+    //   Response response = await dio.get(url, options: Options(headers: header));
+    //   if (response.statusCode! > 199 && response.statusCode! < 300) {
+    //     action(response);
+    //   } else {
+    //     error(response);
+    //   }
+    // }
+    //
+    // if (httpMethod == EHttpMethod.post) {
+    //   Response response = await dio.post(url, data: params, options: Options(headers: header));
+    //   if (response.statusCode! > 199 && response.statusCode! < 300) {
+    //     action(response);
+    //   } else {
+    //     error(response);
+    //   }
+    // }
+    // if (httpMethod == EHttpMethod.put) {
+    //   Response response = await dio.put(url, data: params, options: Options(headers: header));
+    //   if (response.statusCode! > 199 && response.statusCode! < 300) {
+    //     action(response);
+    //   } else {
+    //     error(response);
+    //   }
+    // }
+    //
+    // if (httpMethod == EHttpMethod.patch) {
+    //   Response response = await dio.patch(url, data: params, options: Options(headers: header));
+    //   if (response.statusCode! > 199 && response.statusCode! < 300) {
+    //     action(response);
+    //   } else {
+    //     error(response);
+    //   }
+    // }
+    //
+    // if (httpMethod == EHttpMethod.delete) {
+    //   Response response = await dio.delete(url, options: Options(headers: header));
+    //   if (response.statusCode! > 199 && response.statusCode! < 300) {
+    //     action(response);
+    //   } else {
+    //     error(response);
+    //   }
+    // }
 
-    if (httpMethod == EHttpMethod.get) {
-      Response response = await dio.get(url, options: Options(headers: header));
-      if (response.statusCode! > 199 && response.statusCode! < 300) {
-        action(response);
-      } else {
-        error(response);
-      }
-    }
-
-    if (httpMethod == EHttpMethod.post) {
-      Response response = await dio.post(url, data: params, options: Options(headers: header));
-      if (response.statusCode! > 199 && response.statusCode! < 300) {
-        action(response);
-      } else {
-        error(response);
-      }
-    }
-    if (httpMethod == EHttpMethod.put) {
-      Response response = await dio.put(url, data: params, options: Options(headers: header));
-      if (response.statusCode! > 199 && response.statusCode! < 300) {
-        action(response);
-      } else {
-        error(response);
-      }
-    }
-
-    if (httpMethod == EHttpMethod.patch) {
-      Response response = await dio.patch(url, data: params, options: Options(headers: header));
-      if (response.statusCode! > 199 && response.statusCode! < 300) {
-        action(response);
-      } else {
-        error(response);
-      }
-    }
-
-    if (httpMethod == EHttpMethod.delete) {
-      Response response = await dio.delete(url, options: Options(headers: header));
-      if (response.statusCode! > 199 && response.statusCode! < 300) {
-        action(response);
-      } else {
-        error(response);
-      }
-    }
-
-    // if (httpMethod == EHttpMethod.get) response = await dio.get(url, options: Options(headers: header));
-    // if (httpMethod == EHttpMethod.post) response = await dio.post(url, data: params, options: Options(headers: header));
-    // if (httpMethod == EHttpMethod.put) response = await dio.put(url, data: params, options: Options(headers: header));
-    // if (httpMethod == EHttpMethod.patch) response = await dio.patch(url, data: params, options: Options(headers: header));
-    // if (httpMethod == EHttpMethod.delete) response = await dio.delete(url, options: Options(headers: header));
+    if (httpMethod == EHttpMethod.get) response = await dio.get(url, options: Options(headers: header));
+    if (httpMethod == EHttpMethod.post) response = await dio.post(url, data: params, options: Options(headers: header));
+    if (httpMethod == EHttpMethod.put) response = await dio.put(url, data: params, options: Options(headers: header));
+    if (httpMethod == EHttpMethod.patch) response = await dio.patch(url, data: params, options: Options(headers: header));
+    if (httpMethod == EHttpMethod.delete) response = await dio.delete(url, options: Options(headers: header));
     // ignore: avoid_catches_without_on_clauses
+    if (response.statusCode! > 199 && response.statusCode! < 300) {
+      action(response);
+    } else {
+      error(response);
+    }
   } catch (e) {
-    // error(response);
+    error(response);
   }
-
-  // if (response.statusCode! < 199 && response.statusCode! < 300) {
-  //   action(response);
-  // } else {
-  //   error(response);
-  // }
 }
 
 Future<void> httpGet({
