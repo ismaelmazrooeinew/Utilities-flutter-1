b38560db7c62cd77bccd30d0c6c8bfef7b298d98
Ahmad <ahmadxpanahi@gmail.com>
2022-11-09 19:27:19 +0330

    Edit categoryDatasource

--- lib/data/remote_datasource/category_datasource.dart
+++ lib/data/remote_datasource/category_datasource.dart
@@ -1,6 +1,6 @@
-import 'dart:html';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:utilities/data/dto/category.dart';
 import 'package:utilities/data/dto/generic_response.dart';
 import 'package:utilities/utils/dio_interceptor.dart';
