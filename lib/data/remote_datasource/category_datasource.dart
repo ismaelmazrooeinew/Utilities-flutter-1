3beb9d799c952f2fb13f9f791b33c0e739a728e0
Farzad <104139314+farzaddev65@users.noreply.github.com>
2022-11-27 08:43:36 +0330

    edit 11

--- lib/data/remote_datasource/category_datasource.dart
+++ lib/data/remote_datasource/category_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:utilities/data/dto/category.dart';
 import 'package:utilities/data/dto/generic_response.dart';
 import 'package:utilities/utils/dio_interceptor.dart';
@@ -50,13 +51,13 @@ class CategoryDataSource {
 
   Future<void> delete({
     required final String id,
-    required final Function(GenericResponse) onResponse,
+    required final VoidCallback onResponse,
     required final Function(GenericResponse response) onError,
     final Function(Object error)? failure,
   }) async =>
       httpDelete(
         url: "$baseUrl/Category/$id",
-        action: (Response response) => onResponse(GenericResponse<CategoryReadDto>.fromJson(response.data, fromMap: CategoryReadDto.fromMap)),
+        action: (Response response) => onResponse(),
         error: (Response response) => onError(GenericResponse.fromJson(response.data)),
         failure:(Object error)=> failure!,
       );
