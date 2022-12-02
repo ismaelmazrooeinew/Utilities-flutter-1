d10dd5e98b81ca76a4ae287e751ee12e56300cca
Farzad <104139314+farzaddev65@users.noreply.github.com>
2022-12-02 19:53:57 +0330

    add library 3

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -15,6 +15,8 @@ export 'extensions/shimmer_extension.dart';
 export 'extensions/string_extension.dart';
 export 'extensions/text_extension.dart';
 export 'extensions/widget_extension.dart';
+export 'flip_cart/flip_card.dart';
+export 'flip_cart/flip_card_controller.dart';
 export 'file.dart';
 export 'enums.dart';
 export 'get.dart';
