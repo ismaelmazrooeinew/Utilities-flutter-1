a904cb069c24fc2af2ae06e1b940061d76fa6730
Farzad <104139314+farzaddev65@users.noreply.github.com>
2022-12-06 22:01:03 +0330

    add dotNetWidgets

---
+++ lib/utils/widgets.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:utilities/components/dotted_border.dart';
+import 'package:utilities/utils/get.dart';
+Widget dottedBorder({
+  required final BuildContext context,
+  required final Widget child,
+  final EdgeInsetsGeometry? margin,
+  final EdgeInsetsGeometry? padding,
+  final EdgeInsetsGeometry? paddingDottedBorder,
+  final double? borderRadius,
+  final Color? foregroundColor,
+  final Color? backgroundColor,
+  final Color? dottedBorderColor,
+}) =>
+    Container(
+        margin: margin ?? const EdgeInsets.only(top: 16),
+        padding: padding ?? const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(borderRadius ?? 6),
+          color: foregroundColor ?? Color(0xffFFFFFF),
+        ),
+        child: DottedBorder(
+          color: dottedBorderColor ?? Color(0xff0E4CD5).withOpacity(0.3),
+          strokeWidth: 2,
+          child: Container(
+            width: screenWidth,
+            color: backgroundColor ?? Color(0xffe7eefb),
+            padding: paddingDottedBorder ?? const EdgeInsets.all(8),
+            child: child,
+          ),
+        ));
