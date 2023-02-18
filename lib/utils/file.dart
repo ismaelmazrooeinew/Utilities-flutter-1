14604290c2a360e810b2304f5d479053583a4047
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-02-18 15:57:13 +0330

    add multi Select to filePicker

--- lib/utils/file.dart
+++ lib/utils/file.dart
@@ -1,8 +1,33 @@
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:utilities/utilities.dart';
 
 void showFilePicker({
+  required final Function(List<File> file) action,
+  final FileType fileType = FileType.image,
+  final bool allowMultiple = false,
+  final List<String>? allowedExtensions,
+}) async {
+  final FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: fileType,
+    allowMultiple: allowMultiple,
+    allowedExtensions: allowedExtensions,
+  );
+  if (result != null) {
+    if (allowMultiple) {
+      final List<File> files = <File>[];
+      result.files.forEach((final PlatformFile i) {
+        if (i.path != null) files.add(File(i.path!));
+      });
+      action(files);
+    } else {
+      final File file = File(result.files.single.path!);
+      action(<File>[file]);
+    }
+  }
+}
+
+
+void showMultiFilePicker({
   required final Function(List<File> file) action,
   final FileType fileType = FileType.image,
   final bool allowMultiple = false,
