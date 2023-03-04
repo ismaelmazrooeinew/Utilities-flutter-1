8c138cc12460eb9bbd4bea99739fc3a61351528e
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-03-04 08:01:26 +0330

    add position to video 4

--- lib/components/video_player/video_player_widget.dart
+++ lib/components/video_player/video_player_widget.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flick_video_player/flick_video_player.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:utilities/utilities.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 import 'package:utilities/components/video_player/video_player_controls.dart';
 import 'package:utilities/components/video_player/video_player_data_manager.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
-  CustomVideoPlayer({required this.urls, this.position, Key? key}) : super(key: key);
+  CustomVideoPlayer({required this.urls, Key? key}) : super(key: key);
   List<String> urls;
-  Duration? position;
 
   @override
   _CustomVideoPlayerState createState() => _CustomVideoPlayerState();
@@ -20,21 +17,15 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   late FlickManager flickManager;
   late DataManager dataManager;
   late List<String> urls;
-  Duration position = Duration(milliseconds: 1);
 
   @override
   void initState() {
     super.initState();
     urls = widget.urls;
-    if (widget.position != null) {
-      position = widget.position ?? Duration(milliseconds: 1);
-    }
-
-    VideoPlayerController controls = VideoPlayerController.network(
-      urls[0],
-    );
     flickManager = FlickManager(
-        videoPlayerController: controls,
+        videoPlayerController: VideoPlayerController.network(
+          urls[0],
+        ),
         onVideoEnd: () {
           dataManager.skipToNextVideo(const Duration(seconds: 5));
         });
@@ -54,67 +45,50 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (dataManager.flickManager.flickVideoManager?.videoPlayerController != null) {
-          Duration? position = await dataManager.flickManager.flickVideoManager?.videoPlayerController!.position;
-          Get.back(result: position);
-        } else {
-          Get.back(result: Duration(milliseconds: 1));
-        }
-        return false;
-      },
-      child: Scaffold(
-        body: VisibilityDetector(
-          key: ObjectKey(flickManager),
-          onVisibilityChanged: (visibility) {
-            if (visibility.visibleFraction == 0 && this.mounted) {
-              flickManager.flickControlManager?.autoPause();
-            } else if (visibility.visibleFraction == 1) {
-              flickManager.flickControlManager?.autoResume();
-            }
-          },
-          child: Stack(
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: double.infinity,
-                height: double.infinity,
-                child: FlickVideoPlayer(
-                  flickManager: flickManager,
-                  flickVideoWithControls: FlickVideoWithControls(
-                    videoFit: BoxFit.contain,
+    return Scaffold(
+      body: VisibilityDetector(
+        key: ObjectKey(flickManager),
+        onVisibilityChanged: (visibility) {
+          if (visibility.visibleFraction == 0 && this.mounted) {
+            flickManager.flickControlManager?.autoPause();
+          } else if (visibility.visibleFraction == 1) {
+            flickManager.flickControlManager?.autoResume();
+          }
+        },
+        child: Stack(
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: double.infinity,
+              height: double.infinity,
+              child: FlickVideoPlayer(
+                flickManager: flickManager,
+                flickVideoWithControls: FlickVideoWithControls(
+                  videoFit: BoxFit.contain,
+                  controls: VideoPlayerControls(dataManager: dataManager),
+                ),
+                flickVideoWithControlsFullscreen: FlickVideoWithControls(
+                  controls: FlickVideoWithControls(
                     controls: VideoPlayerControls(dataManager: dataManager),
-                  ),
-                  flickVideoWithControlsFullscreen: FlickVideoWithControls(
-                    controls: FlickVideoWithControls(
-                      controls: VideoPlayerControls(dataManager: dataManager),
-                    ),
                   ),
                 ),
               ),
-              Positioned(
-                top: 0,
-                right: 0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: const [
-                      Text("videoName", style: TextStyle(color: Colors.white, fontSize: 18)),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Icon(
-                        Icons.arrow_forward_rounded,
-                        color: Colors.white,
-                        size: 32,
-                      ),
-                    ],
-                  ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: const [
+                    Text("videoName", style: TextStyle(color: Colors.white, fontSize: 18)),
+                    SizedBox(width: 8,),
+                    Icon(Icons.arrow_forward_rounded, color: Colors.white,size: 32,),
+                  ],
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
