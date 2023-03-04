2ef1e54c24773c8eb5eb7238791a1cc70c42bc58
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-03-04 07:04:29 +0330

    add position to video 2

--- lib/components/video_player/video_player_widget.dart
+++ lib/components/video_player/video_player_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flick_video_player/flick_video_player.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:utilities/utilities.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 import 'package:utilities/components/video_player/video_player_controls.dart';
@@ -18,11 +20,13 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   late FlickManager flickManager;
   late DataManager dataManager;
   late List<String> urls;
+  Duration position = Duration(milliseconds: 1);
 
   @override
   void initState() {
     super.initState();
     urls = widget.urls;
+
     VideoPlayerController controls = VideoPlayerController.network(
       urls[0],
     );
@@ -35,6 +39,9 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     dataManager = DataManager(flickManager: flickManager, urls: urls);
     controls.initialize().then(
       (value) {
+        controls.addListener(() async {
+          position = await controls.position ?? Duration(milliseconds: 1);
+        });
         controls.seekTo(widget.position ?? Duration(milliseconds: 1));
       },
     );
@@ -52,56 +59,62 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: VisibilityDetector(
-        key: ObjectKey(flickManager),
-        onVisibilityChanged: (visibility) {
-          if (visibility.visibleFraction == 0 && this.mounted) {
-            flickManager.flickControlManager?.autoPause();
-          } else if (visibility.visibleFraction == 1) {
-            flickManager.flickControlManager?.autoResume();
-          }
-        },
-        child: Stack(
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: double.infinity,
-              height: double.infinity,
-              child: FlickVideoPlayer(
-                flickManager: flickManager,
-                flickVideoWithControls: FlickVideoWithControls(
-                  videoFit: BoxFit.contain,
-                  controls: VideoPlayerControls(dataManager: dataManager),
-                ),
-                flickVideoWithControlsFullscreen: FlickVideoWithControls(
-                  controls: FlickVideoWithControls(
+    return WillPopScope(
+      onWillPop: () async {
+        Get.back(result: position);
+        return false;
+      },
+      child: Scaffold(
+        body: VisibilityDetector(
+          key: ObjectKey(flickManager),
+          onVisibilityChanged: (visibility) {
+            if (visibility.visibleFraction == 0 && this.mounted) {
+              flickManager.flickControlManager?.autoPause();
+            } else if (visibility.visibleFraction == 1) {
+              flickManager.flickControlManager?.autoResume();
+            }
+          },
+          child: Stack(
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: double.infinity,
+                height: double.infinity,
+                child: FlickVideoPlayer(
+                  flickManager: flickManager,
+                  flickVideoWithControls: FlickVideoWithControls(
+                    videoFit: BoxFit.contain,
                     controls: VideoPlayerControls(dataManager: dataManager),
+                  ),
+                  flickVideoWithControlsFullscreen: FlickVideoWithControls(
+                    controls: FlickVideoWithControls(
+                      controls: VideoPlayerControls(dataManager: dataManager),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              top: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: const [
-                    Text("videoName", style: TextStyle(color: Colors.white, fontSize: 18)),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Icon(
-                      Icons.arrow_forward_rounded,
-                      color: Colors.white,
-                      size: 32,
-                    ),
-                  ],
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: const [
+                      Text("videoName", style: TextStyle(color: Colors.white, fontSize: 18)),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_rounded,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
