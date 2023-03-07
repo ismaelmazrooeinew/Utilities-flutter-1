2df09f48539598eaf4b6ad53715dc24f00caae4c
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-03-08 03:01:56 +0330

    create delete and update to chat

--- lib/data/remote_datasource/chat_datasource.dart
+++ lib/data/remote_datasource/chat_datasource.dart
@@ -3,7 +3,6 @@ import 'package:utilities/data/dto/chat.dart';
 import 'package:utilities/data/dto/generic_response.dart';
 import 'package:utilities/utils/dio_interceptor.dart';
 
-
 class ChatDataSource {
   final String baseUrl;
 
@@ -16,6 +15,20 @@ class ChatDataSource {
     final Function(String error)? failure,
   }) async =>
       httpPost(
+        url: "$baseUrl/Chat",
+        body: dto,
+        action: (Response response) => onResponse(GenericResponse<ChatReadDto>.fromJson(response.data, fromMap: ChatReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse.fromJson(response.data)),
+        failure: failure,
+      );
+
+  Future<void> update({
+    required final ChatCreateDto dto,
+    required final Function(GenericResponse<ChatReadDto>) onResponse,
+    required final Function(GenericResponse response) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpPut(
         url: "$baseUrl/Chat",
         body: dto,
         action: (Response response) => onResponse(GenericResponse<ChatReadDto>.fromJson(response.data, fromMap: ChatReadDto.fromMap)),
@@ -43,6 +56,19 @@ class ChatDataSource {
   }) async =>
       httpGet(
         url: "$baseUrl/Chat/$userId",
+        action: (Response response) => onResponse(GenericResponse<ChatReadDto>.fromJson(response.data, fromMap: ChatReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse.fromJson(response.data)),
+        failure: failure,
+      );
+
+  Future<void> delete({
+    required final String chatId,
+    required final Function(GenericResponse<ChatReadDto>) onResponse,
+    required final Function(GenericResponse response) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpDelete(
+        url: "$baseUrl/Chat?id=$chatId",
         action: (Response response) => onResponse(GenericResponse<ChatReadDto>.fromJson(response.data, fromMap: ChatReadDto.fromMap)),
         error: (Response response) => onError(GenericResponse.fromJson(response.data)),
         failure: failure,
