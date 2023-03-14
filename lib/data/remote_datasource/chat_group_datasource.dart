d052f87226e53b80b6eb83635028f2dda0ddfaf1
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-03-14 09:16:31 +0330

    chat group 8

--- lib/data/remote_datasource/chat_group_datasource.dart
+++ lib/data/remote_datasource/chat_group_datasource.dart
@@ -37,6 +37,22 @@ class ChatGroupDataSource {
         failure: failure,
       );
 
+    Future<void> updateMessage({
+    required final CreateGroupMessage dto,
+    required final Function(GenericResponse<ChatGroupReadDto>) onResponse,
+    required final Function(GenericResponse response) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpPut(
+        url: "$baseUrl/Chat/UpdateGroupChatMessage",
+        body: dto,
+        action: (Response response) => onResponse(GenericResponse<ChatGroupReadDto>.fromJson(response.data, fromMap: ChatGroupReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse.fromJson(response.data)),
+        failure: failure,
+      );
+
+
+
   Future<void> update({
     required final ChatGroupCreateUpdateDto dto,
     required final Function(GenericResponse<ChatGroupReadDto>) onResponse,
@@ -108,9 +124,25 @@ class ChatGroupDataSource {
     final Function(String error)? failure,
   }) async =>
       httpDelete(
+        url: "$baseUrl/Chat/?id=$chatId",
+        action: (Response response) => onResponse(GenericResponse<String>.fromJson(response.data, fromMap: ChatReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse.fromJson(response.data)),
+        failure: failure,
+      );
+
+
+  Future<void> deleteMessage({
+    required final String chatId,
+    required final Function(GenericResponse) onResponse,
+    required final Function(GenericResponse response) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpDelete(
         url: "$baseUrl/Chat?id=$chatId",
         action: (Response response) => onResponse(GenericResponse<String>.fromJson(response.data, fromMap: ChatReadDto.fromMap)),
         error: (Response response) => onError(GenericResponse.fromJson(response.data)),
         failure: failure,
       );
+
+
 }
