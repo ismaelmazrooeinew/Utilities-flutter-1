d2ca4aff40f264414a7c402fff1f8e02ee799666
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-03-31 09:38:42 +0330

    edit mail_sms_notification.dart 1

--- lib/data/dto/mail_sms_notification.dart
+++ lib/data/dto/mail_sms_notification.dart
@@ -4,18 +4,22 @@ class CreateMailSmsNotificationDto {
   CreateMailSmsNotificationDto({
     this.userIds,
     this.title,
+    this.bigTitle,
     this.content,
     this.bigContent,
     this.url,
     this.actionType,
+    this.showForeground,
   });
 
   final List<String>? userIds;
   final String? title;
+  final String? bigTitle;
   final String? content;
   final String? bigContent;
   final String? url;
   final String? actionType;
+  final bool? showForeground;
 
   factory CreateMailSmsNotificationDto.fromJson(String str) => CreateMailSmsNotificationDto.fromMap(json.decode(str));
 
@@ -24,18 +28,22 @@ class CreateMailSmsNotificationDto {
   factory CreateMailSmsNotificationDto.fromMap(Map<String, dynamic> json) => CreateMailSmsNotificationDto(
         userIds: json["userIds"] == null ? [] : List<String>.from(json["userIds"]!.map((x) => x)),
         title: json["title"],
+        bigTitle: json["bigTitle"],
         content: json["content"],
         bigContent: json["bigContent"],
         url: json["url"],
         actionType: json["actionType"],
+        showForeground: json["showForeground"],
       );
 
   Map<String, dynamic> toMap() => {
         "userIds": userIds == null ? [] : List<String>.from(userIds!.map((x) => x)),
         "title": title,
+        "bigTitle": bigTitle,
         "content": content,
         "bigContent": bigContent,
         "url": url,
         "actionType": actionType,
+        "showForeground": showForeground,
       };
 }
